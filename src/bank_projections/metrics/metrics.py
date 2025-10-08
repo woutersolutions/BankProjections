b6e50936b7ab6d571032deba587d0edd75885ecd
Wouter van Krieken <woutervankrieken@gmail.com>
2025-10-08 18:04:00 +0200

    Enhance balance sheet item functionality by adding logical operators for combining items and registering new item types for improved categorization

--- src/bank_projections/metrics/metrics.py
+++ src/bank_projections/metrics/metrics.py
@@ -24,6 +24,52 @@ class Metric(ABC):
     def calculate(self, bs: BalanceSheet, previous_metrics: dict[str, float]) -> float:
         pass
 
+    def __neg__(self):
+        return Multiplied(f"-{self.name}", self, -1)
+
+class Ratio(Metric):
+    def __init__(self, name: str, numerator: Metric, denominator: Metric):
+        super().__init__(name)
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def calculate(self, bs: BalanceSheet, previous_metrics: dict[str, float]) -> float:
+
+        numerator = self.numerator.calculate(bs, previous_metrics)
+        denominator = self.denominator.calculate(bs, previous_metrics)
+        return numerator / denominator if denominator != 0 else None
+
+class Sum(Metric):
+    def __init__(self, name: str, metrics: list[Metric]):
+        super().__init__(name)
+        self.metrics = metrics
+
+    def calculate(self, bs: BalanceSheet, previous_metrics: dict[str, float]) -> float:
+        return sum(metric.calculate(bs, previous_metrics) for metric in self.metrics)
+
+class Clipped(Metric):
+    def __init__(self, name: str, metric: Metric, min_value: float = None, max_value: float = None):
+        super().__init__(name)
+        self.metric = metric
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def calculate(self, bs: BalanceSheet, previous_metrics: dict[str, float]) -> float:
+        value = self.metric.calculate(bs, previous_metrics)
+        if self.min_value is not None:
+            value = max(value, self.min_value)
+        if self.max_value is not None:
+            value = min(value, self.max_value)
+        return value
+
+class Multiplied(Metric):
+    def __init__(self, name: str, metric: Metric, factor: float):
+        super().__init__(name)
+        self.metric = metric
+        self.factor = factor
+
+    def calculate(self, bs: BalanceSheet, previous_metrics: dict[str, float]) -> float:
+        return self.metric.calculate(bs, previous_metrics) * self.factor
 
 class BalanceSheetAggregation(Metric):
     def __init__(self, name: str, metric: str, item: BalanceSheetItem = BalanceSheetItem()):
@@ -49,4 +95,11 @@ MetricRegistry.register(
         | (BalanceSheetItemRegistry.get("Derivatives") & BalanceSheetItemRegistry.get("Positive")),
     ),
 )
-
+MetricRegistry.register(
+    "cet1",
+    -BalanceSheetAggregation(
+        "CET1 capital",
+        "Book value",
+        BalanceSheetItem(ItemType="CET1 capital"),
+    ),
+)
